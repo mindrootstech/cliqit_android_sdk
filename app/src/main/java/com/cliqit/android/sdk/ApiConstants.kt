87c26db2b8f443b0579ff163c67c0db91111c2d2
anilkumar <project4mindroots@gmail.com>
2026-08-26 16:18:08 +0530

    Update API base URL

--- app/src/main/java/com/cliqit/android/sdk/ApiConstants.kt
+++ app/src/main/java/com/cliqit/android/sdk/ApiConstants.kt
@@ -1,7 +1,7 @@
 package com.cliqit.android.sdk
 
 object ApiConstants {
-    const val BASE_URL = "https://api.theblockyapp.com/api/v1/sdk/"
+    const val BASE_URL = "https://api.gocliqit.com/api/v1/sdk/"
     const val MATCH = "app/match"
     const val VERIFY = "verify"
     const val EVENT = "event"
