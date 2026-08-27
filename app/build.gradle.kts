7bcdc3eb3a400c0336ec174a21dad391d4ace5d6
anilkumar <project4mindroots@gmail.com>
2026-08-27 11:16:47 +0530

    Update API base URL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.cliqit"
                 artifactId = "deeplink-sdk"
-                version = "1.0.0"
+                version = "1.0.5" // <--- Update this to match your tag
             }
         }
     }
